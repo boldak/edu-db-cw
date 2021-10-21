4f3c0833418f42caf8e8e626fd77609e2a87152c
Ilya Senchukov <senchukov02@gmail.com>
2021-10-21 15:59:58 +0300

    fix: change repo owner

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -76,7 +76,7 @@ module.exports = {
 
     lastUpdated: 'Останнє оновлення', // string | boolean
       // Assumes GitHub. Can also be a full GitLab url.
-    repo: 'https://github.com/boldak/edu-db-cw',
+    repo: 'https://github.com/Beaxhem/edu-db-cw',
     // Customising the header label
     // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
     repoLabel: 'Github',
