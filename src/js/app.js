28026dc19e808cffc46c254ca93e7b61f311cbae
Bogdan-Zinovij <bogdanolexandrov@gmail.com>
2021-12-24 13:59:24 +0200

    added functionality for availableFor table

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const dataSetRouter = require('./routes/dataSetsRoutes');
+const availableForRouter = require('./routes/availableForRoutes');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/v1/dataSets', dataSetRouter);
+app.use('/api/v1/availableFor', availableForRouter);
 
 module.exports = app;
