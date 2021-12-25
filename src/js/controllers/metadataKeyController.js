f3706e7b78852f33dd4993b9c355c3ae233f1221
artemmatiushenko1 <artom.matyushenko@gmail.com>
2021-12-25 13:03:33 +0200

    Created function for getting all metadata keys

--- src/js/controllers/metadataKeyController.js
+++ src/js/controllers/metadataKeyController.js
@@ -1,6 +1,25 @@
 'use strict';
 
-exports.getAllMetadataKeys = async (req, reply) => {};
+const MetadataKey = require('../db/models/MetaDataKey');
+
+exports.getAllMetadataKeys = async (req, reply) => {
+  try {
+    const metadataKeys = await MetadataKey.findAll();
+    reply.status(200).send({
+      status: 'success',
+      results: metadataKeys.length,
+      data: {
+        metadataKeys,
+      },
+    });
+  } catch (err) {
+    reply.status(404).send({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
+
 exports.createMetadataKey = async (req, reply) => {};
 exports.getMetadataKey = async (req, reply) => {};
 exports.deleteMetadataKey = async (req, reply) => {};
