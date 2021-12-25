54e3231b2a29aa067da8be653b8e32062880b3a2
artemmatiushenko1 <artom.matyushenko@gmail.com>
2021-12-25 14:48:51 +0200

    Registered routes for metadatakey table (closes #55)

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ const Fastify = require('fastify');
 const availableForRouter = require('./routes/availableForRoutes');
 const categoryRouter = require('./routes/categoriesRoutes');
 const dataSetRouter = require('./routes/dataSetsRoutes');
+const metadataKeyRouter = require('./routes/metadataKeyRoutes');
 
 const app = Fastify({
   logger: true,
@@ -12,5 +13,6 @@ const app = Fastify({
 app.register(dataSetRouter, { prefix: '/api/v1/dataSets' });
 app.register(categoryRouter, { prefix: '/api/v1/categories' });
 app.register(availableForRouter, { prefix: '/api/v1/availableFor' });
+app.register(metadataKeyRouter, { prefix: '/api/v1/metadataKey' });
 
 module.exports = app;
