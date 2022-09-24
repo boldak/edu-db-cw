605ce2b5519ce5031043f97a2ecc662c6e5e0b34
MrSampy <mynameisserzheo@gmail.com>
2022-09-24 14:40:40 +0300

    Change `title` in `config.js`

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -95,7 +95,7 @@ module.exports = {
     // editLinkText: 'Ви можете покращити цю сторінку'
 
   },
-  title: 'Назва проєкту',
+  title: 'Система управління проектами',
   description: 'Курсова робота з дисципліни "Бази даних"',
   configureWebpack: {
     resolve: {
